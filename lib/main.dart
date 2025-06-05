c217de91143bdba092190adac7cb0601e3623e5d
Bbell <428bbell@gmail.com>
2025-06-06 01:46:03 +0900

    [Feat]: 파이어베이스 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:runlog/firebase_options.dart';
 import 'package:runlog/router.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
