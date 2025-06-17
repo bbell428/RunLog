d001776e984479e6557696c69305d14fa37286bf
Bbell <428bbell@gmail.com>
2025-06-17 19:11:08 +0900

    [Feat]: 로그아웃 기능

--- lib/View/Home/home_view.dart
+++ lib/View/Home/home_view.dart
@@ -18,64 +18,74 @@ class _MyWidgetState extends State<HomeView> {
   Widget build(BuildContext context) {
     final authState = context.read<AuthBloc>().state;
 
-    return Scaffold(
-      appBar: AppBar(title: Text('런닝')),
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is Unauthenticated) {
+          context.go('/login'); // ✅ 상태가 바뀐 후에 이동
+        }
+      },
 
-      body: Container(
-        padding: const EdgeInsets.all(50),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 200,
-                child: Image.asset('assets/images/running.png'),
-              ),
-              Text(
-                '마라톤 대비',
-                style: TextStyle(fontSize: Design.screenWidth(context) * 0.08),
-              ),
-              Text(
-                '운동을 시작하려면 아래 버튼을 눌러보세요',
-                style: TextStyle(
-                  fontSize: Design.screenWidth(context) * 0.04,
-                  color: Colors.grey[600],
+      child: Scaffold(
+        appBar: AppBar(title: Text('런닝')),
+
+        body: Container(
+          padding: const EdgeInsets.all(50),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: Image.asset('assets/images/running.png'),
                 ),
-              ),
-              SizedBox(height: Design.screenHeight(context) * 0.02),
-              SizedBox(
-                width: Design.screenWidth(context) * 0.6,
-                height: Design.screenHeight(context) * 0.08,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  onPressed: () => context.push('/runningMap'), // 스택으로
-                  child: Text(
-                    '런닝하기',
-                    style: TextStyle(
-                      fontSize: Design.screenWidth(context) * 0.07,
-                    ),
+                Text(
+                  '마라톤 대비',
+                  style: TextStyle(
+                    fontSize: Design.screenWidth(context) * 0.08,
                   ),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<AuthBloc>().add(SignOutRequested());
-                },
-                child: const Text("로그아웃"),
-              ),
-              if (authState is Authenticated) ...[
-                SizedBox(height: 10),
-                Text('이메일: ${authState.user.email}'),
-                Text('이름: ${authState.user.name}'),
+                Text(
+                  '운동을 시작하려면 아래 버튼을 눌러보세요',
+                  style: TextStyle(
+                    fontSize: Design.screenWidth(context) * 0.04,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                SizedBox(height: Design.screenHeight(context) * 0.02),
+                SizedBox(
+                  width: Design.screenWidth(context) * 0.6,
+                  height: Design.screenHeight(context) * 0.08,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () => context.push('/runningMap'), // 스택으로
+                    child: Text(
+                      '런닝하기',
+                      style: TextStyle(
+                        fontSize: Design.screenWidth(context) * 0.07,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<AuthBloc>().add(SignOutRequested());
+                  },
+                  child: const Text("로그아웃"),
+                ),
+                if (authState is Authenticated) ...[
+                  SizedBox(height: 10),
+                  Text('이메일: ${authState.user.email}'),
+                  Text('이름: ${authState.user.name}'),
+                ],
+                SizedBox(height: Design.screenHeight(context) * 0.1),
               ],
-              SizedBox(height: Design.screenHeight(context) * 0.1),
-            ],
+            ),
           ),
         ),
       ),
