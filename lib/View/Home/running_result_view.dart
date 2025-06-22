c12c442356a79ba567a87d41b8ec26f4257f531d
Bbell <428bbell@gmail.com>
2025-06-22 16:54:11 +0900

    [Feat]: showSnackBar - 러닝 기록 최종 확인

--- lib/View/Home/running_result_view.dart
+++ lib/View/Home/running_result_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:runlog/bloc/bloc/auth_bloc.dart';
 import 'package:runlog/bloc/bloc/running_result_bloc.dart';
 import 'package:runlog/bloc/event/running_result_event.dart';
 import 'package:runlog/bloc/state/auth_state.dart';
+import 'package:runlog/bloc/state/running_result_state.dart';
 
 class RunningResultView extends StatelessWidget {
   final double distance; // km
@@ -21,25 +23,39 @@ class RunningResultView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text('런닝 결과')),
-      body: Column(
-        children: [
-          Text('거리: ${distance.toStringAsFixed(1)} m'),
-          Text('시간: ${duration.inMinutes}분 ${duration.inSeconds % 60}초'),
-          ElevatedButton(
-            onPressed: () {
-              if (authState is Authenticated) {
-                context.read<RunningResultBloc>().add(
-                  SaveRunningResultEvent(
-                    uid: authState.user.uid,
-                    distance: distance,
-                    duration: duration,
-                  ),
-                );
-              }
-            },
-            child: const Text("기록하기"),
-          ),
-        ],
+      body: BlocListener<RunningResultBloc, RunningResultState>(
+        listener: (context, state) {
+          if (state is RunningResultSaved) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('기록 완료')),
+            );
+            context.go('/home');
+          } else if (state is RunningResultError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.message)),
+            );
+          }
+        },
+        child: Column(
+          children: [
+            Text('거리: ${distance.toStringAsFixed(2)} km'),
+            Text('시간: ${duration.inMinutes}분 ${duration.inSeconds % 60}초'),
+            ElevatedButton(
+              onPressed: () {
+                if (authState is Authenticated) {
+                  context.read<RunningResultBloc>().add(
+                    SaveRunningResultEvent(
+                      uid: authState.user.uid,
+                      distance: distance,
+                      duration: duration,
+                    ),
+                  );
+                }
+              },
+              child: const Text("기록하기"),
+            ),
+          ],
+        ),
       ),
     );
   }
